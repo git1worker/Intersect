5b6b2185ddf712dc7bc3299382d1ab168f33531d
Ilya Rudenko <72783330+git1worker@users.noreply.github.com>
2023-09-09 23:26:49 +0300

    Update hard.cpp

--- hard.cpp
+++ hard.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-struct Vector {
-    Vector(float x, float y) : x(x), y(y) {}
+struct Vector2f {
+    Vector2f(float x, float y) : x(x), y(y) {}
     float x;
     float y;
 };
 
 struct Ray {
-    Ray(Vector pos, Vector direction) : pos(pos), direction(direction) {}
-    Vector pos;
-    Vector direction;
+    Ray(Vector2f pos, Vector2f direction) : pos(pos), direction(direction) {}
+    Vector2f pos;
+    Vector2f direction;
 };
 
 struct Section {
@@ -18,11 +18,11 @@ struct Section {
     float x2, y2;
 };
 
-bool isPointOnTheRay(Vector point, Ray ray) {
+bool isPointOnTheRay(Vector2f point, Ray ray) {
     // Нужна проверка, если линия вертикальная, то есть если ray.direction.x == 0
-    if (ray.direction.x <= 0.001 && ray.direction.x >= -0.001) {
+    if (ray.direction.x <= 0.1 && ray.direction.x >= -0.1) {
         // Если точка принадлежит прямой
-        if (point.x + 0.001 >= ray.pos.x && point.x - 0.001 <= ray.pos.x) {
+        if (point.x + 0.1 >= ray.pos.x && point.x - 0.1 <= ray.pos.x) {
             // Луч направлен в вверх относительно y
             if (ray.direction.y > 0) {
                 if (point.y > ray.pos.y)
@@ -41,23 +41,23 @@ bool isPointOnTheRay(Vector point, Ray ray) {
         float assumedY = k * point.x + b;
         float assumedX = (point.y - b) / k;
         // Если координаты У совпадают у точки и луча
-        if (point.y + 0.001 >= assumedY && point.y - 0.001 <= assumedY) {
+        if (point.y + 0.1 >= assumedY && point.y - 0.1 <= assumedY) {
             // График стремится вниз относительно y
-            if (ray.direction.y < -0.001) {
+            if (ray.direction.y < -0.1) {
                 if (assumedY > ray.pos.y)
                     return false;
                 else
                     return true;
             }
             // График стремится вверх относительно у
-            if (ray.direction.y > 0.001) {
+            if (ray.direction.y > 0.1) {
                 if (assumedY < ray.pos.y)
                     return false;
                 else
                     return true;
             }
             // График постоянен
-            if (ray.direction.y < 0.001 && ray.direction.y > -0.001) {
+            if (ray.direction.y < 0.1 && ray.direction.y > -0.1) {
                 // График направлен в сторону возрастания относительно х
                 if (ray.direction.x > 0) {
                     if (assumedX < ray.pos.x)
@@ -79,11 +79,55 @@ bool isPointOnTheRay(Vector point, Ray ray) {
     return false;
 }
 
-bool isPointOnTheSection(Vector point, Section sect){
+
+
+bool intersectionRayAndSection(const Ray& ray, const Section& sect){
+    // Если и луч и отрезок вертикальные
+    if (ray.direction.x <= 0.1 && ray.direction.x >= -0.1 && std::abs(sect.x1 - sect.x2) <= 0.1 && std::abs(sect.x1 - sect.x2) >= -0.1)
+        return true;
+    // Если луч вертикальный
+	if (ray.direction.x <= 0.1 && ray.direction.x >= -0.1){
+	    float kS = (sect.y1 - sect.y2) / (sect.x1 - sect.x2);
+    	float bS = sect.y1 - kS * sect.x1;
+    	float intersectY = kS * ray.pos.x + bS;
+    	Vector2f p{ray.pos.x, intersectY};
+    	if (isPointOnTheSection(p, sect) && isPointOnTheRay(p, ray))
+    	    return true;
+	}
+	// Если отрезок вертикальный
+	if (std::abs(sect.x1 - sect.x2) <= 0.1 && std::abs(sect.x1 - sect.x2) >= -0.1)
+	{
+	    float kR = ray.direction.y / ray.direction.x;
+    	float bR = ray.pos.y - kR * ray.pos.x;
+    	float intersectY = kR * sect.x1 + bR;
+    	Vector2f p{sect.x1, intersectY};
+    	if (isPointOnTheSection(p, sect) && isPointOnTheRay(p, ray))
+    	    return true;
+	}
+	else{
+	    
+	    float kS = (sect.y1 - sect.y2) / (sect.x1 - sect.x2);
+    	float bS = sect.y1 - kS * sect.x1;
+    	float kR = ray.direction.y / ray.direction.x;
+    	float bR = ray.pos.y - kR * ray.pos.x;
+        // 	std::cout << kR << ' ' << bR << ' '
+        // 	<< kS << ' ' << bS << std::endl;
+    	if (std::abs(kS - kR) >= 0.1 || std::abs(kS - kR) <= -0.1){
+    	    float intersectX = (bR - bS) / (kS - kR);
+        	float intersectY = kS * intersectX + bS;
+        	Vector2f p{intersectX, intersectY};
+        	if (isPointOnTheSection(p, sect) && isPointOnTheRay(p, ray))
+        	    return true;
+    	}
+	}
+	return false;
+}
+
+bool isPointOnTheSection(Vector2f point, Section sect){
     // Нужна проверка, если линия вертикальная, то есть если ray.direction.x == 0
-    if (std::abs(sect.x1 - sect.x2) <= 0.001 && std::abs(sect.x1 - sect.x2) >= -0.001){
+    if (std::abs(sect.x1 - sect.x2) <= 0.1 && std::abs(sect.x1 - sect.x2) >= -0.1){
         // Если точка принадлежит прямой
-        if (point.x + 0.001 >= sect.x1 && point.x - 0.001 <= sect.x1){
+        if (point.x + 0.1 >= sect.x1 && point.x - 0.1 <= sect.x1){
             // Если точка принадлежит отрезку
             if ((point.y >= sect.y1 && point.y <= sect.y2) || (point.y >= sect.y2 && point.y <= sect.y1)){
                 return true;
@@ -96,82 +140,37 @@ bool isPointOnTheSection(Vector point, Section sect){
     	float assumedY = k * point.x + b;
     	float assumedX = (point.y - b) / k;
     	// Если координаты У совпадают у точки и прямой
-    	if (point.y + 0.001 >= assumedY && point.y - 0.001 <= assumedY)
+    	if (point.y + 0.1 >= assumedY && point.y - 0.1 <= assumedY)
     	{   
-    	    if ((point.y >= sect.y1 && point.y <= sect.y2) || (point.y >= sect.y2 && point.y <= sect.y1)){
-    	        if ((point.x >= sect.x1 && point.x <= sect.x2) || (point.x >= sect.x2 && point.x <= sect.x1))
-    	            return true;
-    	    }    
+	        if ((point.x >= sect.x1 && point.x <= sect.x2) || (point.x >= sect.x2 && point.x <= sect.x1))
+	            return true;
     	}
     	
     }
 	
 	return false;
 }
-
-bool intersectionRayAndSection(const Ray& ray, const Section& sect){
-    // Если и луч и отрезок вертикальные
-    if (ray.direction.x <= 0.001 && ray.direction.x >= -0.001 && std::abs(sect.x1 - sect.x2) <= 0.001 && std::abs(sect.x1 - sect.x2) >= -0.001)
-        return true;
-    // Если луч вертикальный
-	if (ray.direction.x <= 0.001 && ray.direction.x >= -0.001){
-	    float kS = (sect.y1 - sect.y2) / (sect.x1 - sect.x2);
-    	float bS = sect.y1 - kS * sect.x1;
-    	float intersectY = kS * ray.pos.x + bS;
-    	Vector p{ray.pos.x, intersectY};
-    	if (isPointOnTheSection(p, sect) && isPointOnTheRay(p, ray))
-    	    return true;
-	}
-	// Если отрезок вертикальный
-	if (std::abs(sect.x1 - sect.x2) <= 0.001 && std::abs(sect.x1 - sect.x2) >= -0.001)
-	{
-	    float kR = ray.direction.y / ray.direction.x;
-    	float bR = ray.pos.y - kR * ray.pos.x;
-    	float intersectY = kR * sect.x1 + bR;
-    	Vector p{sect.x1, intersectY};
-    	if (isPointOnTheSection(p, sect) && isPointOnTheRay(p, ray))
-    	    return true;
-	}
-	else{
-	    
-	    float kS = (sect.y1 - sect.y2) / (sect.x1 - sect.x2);
-    	float bS = sect.y1 - kS * sect.x1;
-    	float kR = ray.direction.y / ray.direction.x;
-    	float bR = ray.pos.y - kR * ray.pos.x;
-        // 	std::cout << kR << ' ' << bR << ' '
-        // 	<< kS << ' ' << bS << std::endl;
-    	if (std::abs(kS - kR) >= 0.001 || std::abs(kS - kR) <= -0.001){
-    	    float intersectX = (bR - bS) / (kS - kR);
-        	float intersectY = kS * intersectX + bS;
-        	Vector p{intersectX, intersectY};
-        	if (isPointOnTheSection(p, sect) && isPointOnTheRay(p, ray))
-        	    return true;
-    	}
-	}
-	return false;
-}
-
-bool intersectionSectionAndSection(const Section& sect1, const Section& sect2){
+bool intersectionSectionAndSection(const Section sect1, const Section sect2){
     // Если отрезок1 и отрезок2 вертикальные
-    if (std::abs(sect1.x1 - sect1.x2) <= 0.001 && std::abs(sect1.x1 - sect1.x2) >= -0.001 && 
-    std::abs(sect2.x1 - sect2.x2) <= 0.001 && std::abs(sect2.x1 - sect2.x2) >= -0.001)
+    if (std::abs(sect1.x1 - sect1.x2) <= 0.1 && std::abs(sect1.x1 - sect1.x2) >= -0.1 && 
+    std::abs(sect2.x1 - sect2.x2) <= 0.1 && std::abs(sect2.x1 - sect2.x2) >= -0.1)
         return false;
     // Если отрезок1 вертикальный
-	else if (std::abs(sect1.x1 - sect1.x2) <= 0.001 && std::abs(sect1.x1 - sect1.x2) >= -0.001){
+	else if (std::abs(sect1.x1 - sect1.x2) <= 0.1 && std::abs(sect1.x1 - sect1.x2) >= -0.1){
 	    float kS = (sect2.y1 - sect2.y2) / (sect2.x1 - sect2.x2);
     	float bS = sect2.y1 - kS * sect2.x1;
     	float intersectY = kS * sect1.x1 + bS;
-    	Vector p{sect1.x1, intersectY};
+    	Vector2f p{sect1.x1, intersectY};
     	if (isPointOnTheSection(p, sect1) && isPointOnTheSection(p, sect2))
     	    return true;
 	}
 	// Если отрезок2 вертикальный
-	else if (std::abs(sect2.x1 - sect2.x2) <= 0.001 && std::abs(sect2.x1 - sect2.x2) >= -0.001)
+	else if (std::abs(sect2.x1 - sect2.x2) <= 0.1 && std::abs(sect2.x1 - sect2.x2) >= -0.1)
 	{
 	    float kS = (sect1.y1 - sect1.y2) / (sect1.x1 - sect1.x2);
     	float bS = sect1.y1 - kS * sect1.x1;
     	float intersectY = kS * sect2.x1 + bS;
-    	Vector p{sect2.x1, intersectY};
+    	Vector2f p{sect2.x1, intersectY};
     	if (isPointOnTheSection(p, sect1) && isPointOnTheSection(p, sect2))
     	    return true;
 	}
@@ -182,10 +181,10 @@ bool intersectionSectionAndSection(const Section& sect1, const Section& sect2){
     	float bS2 = sect2.y1 - kS2 * sect2.x1;
         // 	std::cout << kR << ' ' << bR << ' '
         // 	<< kS << ' ' << bS << std::endl;
-    	if (std::abs(kS1 - kS2) >= 0.001 || std::abs(kS1 - kS2) <= -0.001){
+    	if (std::abs(kS1 - kS2) >= 0.1 || std::abs(kS1 - kS2) <= -0.1){
     	    float intersectX = (bS2 - bS1) / (kS1 - kS2);
         	float intersectY = kS1 * intersectX + bS1;
-        	Vector p{intersectX, intersectY};
+        	Vector2f p{intersectX, intersectY};
         	if (isPointOnTheSection(p, sect1) && isPointOnTheSection(p, sect2))
         	    return true;
     	}
@@ -195,12 +194,10 @@ bool intersectionSectionAndSection(const Section& sect1, const Section& sect2){
 
 int main()
 {   
-    // Y - down, X - right
-	Section a{-3, 6, 3, -3};
-	Section b{0, 7, 1, 10};
-	Vector point{2, 4};
+	Section s1{1408, 192, 1440, 224};
+	Section s2{1424, 142, 1419, 513.7897};
 	
-    std::cout << intersectionSectionAndSection(a, b);
+	cout<<intersectionSectionAndSection(s1, s2)<< std::endl;
     
 	return 0;
 	
